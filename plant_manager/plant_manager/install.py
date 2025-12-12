8eaeda355b26362abd7e5e156099d02f79cb4522
frappe <frappe@BhargavHP.localdomain>
2025-12-12 13:59:28 +0000

    main

--- plant_manager/plant_manager/install.py
+++ plant_manager/plant_manager/install.py
@@ -11,6 +11,15 @@ def after_install():
     create_default_operation_status("Returned Without Operation")
     create_default_operation_status("Completed")
 
+    # Process Operation Types
+    create_default_process_operation_type("Stage Inspection")
+    create_default_process_operation_type("Final Inspection")
+    create_default_process_operation_type("FG")
+    create_default_process_operation_type("Reject")
+    create_default_process_operation_type("Rework")
+    create_default_process_operation_type("Manual Operation")
+    create_default_process_operation_type("Machine Operation")
+
 
 # Operation Status for Material In, Material Out & In-House Production
 def create_default_operation_status(doc_name):
@@ -18,6 +27,15 @@ def create_default_operation_status(doc_name):
         frappe.get_doc({
             "doctype": "Operation Status",
             "operation_status": doc_name,
+            "title": doc_name
+        }).insert(ignore_permissions=True)
+        
+# Process Operation Types
+def create_default_process_operation_type(doc_name):
+    if  not frappe.db.exists("Process Operation Type", "doc_name"):   
+        frappe.get_doc({
+            "doctype": "Process Operation Type",
+            "operation_type": doc_name,
             "title": doc_name
         }).insert(ignore_permissions=True)
 
