36c601c6727f700df8801407b6c4d3cff02a84e8
bhargav007n <bhargav007n@gmail.com>
2026-04-01 12:42:44 +0000

    main

---
+++ plant_manager/plant_manager/doctype/work_station_type/work_station_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Bhargav N and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Work Station Type", {
+// 	refresh(frm) {
+
+// 	},
+// });
